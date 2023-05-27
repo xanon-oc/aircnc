f3139b15ae832d881abbbfc69b33d828173e08de
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-05-27 14:35:32 +0600

    category box added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Shared/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
 
 const Main = () => {
   return (
     <div>
       <Navbar />
+      <div className="pt-28 pb-20">
+        <Outlet />
+      </div>
     </div>
   );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
